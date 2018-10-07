9137f262a411858409f619fd0180fe2712ff8ae3
Wenming Ma <mwmcnpc@126.com>
2018-10-07 17:03:14 +0800

    Update Net.py

--- Tutorial/Codes/PytorchMF/Net.py
+++ Tutorial/Codes/PytorchMF/Net.py
@@ -32,24 +32,25 @@ class Net(nn.Module):
 
 def loadData(path):
     data = pd.DataFrame(pd.read_table(path,header=None))
-    users=torch.tensor(data.iloc[:,0].values)
-    items=torch.tensor(data.iloc[:,0].values)
+    users=torch.tensor(data.iloc[:,0].values)-1
+    items=torch.tensor(data.iloc[:,0].values)-1
     scores=torch.tensor(data.iloc[:,2].values,dtype=torch.float32)    
     dataset = TensorDataset(users,items, scores)
-    loader = DataLoader(dataset, batch_size = 64, shuffle = True)
+    loader = DataLoader(dataset, batch_size = 16, shuffle = True)
     return loader
 
 if __name__ == "__main__":
     net=Net()
-    loader=loadData('./ml-100k/u1.base')
-    optimizer = optim.SGD(net.parameters(), lr=0.01,momentum = 0.9)
+    train_loader=loadData('./ml-100k/u1.base')
+    test_loader=loadData('./ml-100k/u1.base')
+    optimizer = optim.SGD(net.parameters(), lr=0.005)
     index=0
     #lambda_l2=0.001
     mse = nn.MSELoss()
     for epoch in range (1,50):
         totloss=0
         num=0
-        for i_batch, sample_batched in enumerate(loader):        
+        for i_batch, sample_batched in enumerate(train_loader):        
             optimizer.zero_grad()   # zero the gradient buffers
             output=net(sample_batched[0],sample_batched[1])
             target=sample_batched[2]
@@ -64,8 +65,27 @@ if __name__ == "__main__":
             totloss+=loss.data.numpy()*size
             
            
-        print np.mean(totloss/num)
-        print '\n'
+        meanloss=np.mean(totloss/num)
+        rmse=np.sqrt(meanloss)
+        print meanloss, rmse,'\n'
+    totloss=0
+    num=0
+    for i_batch, sample_batched in enumerate(test_loader):        
+        output=net(sample_batched[0],sample_batched[1])
+        target=sample_batched[2]
+        loss = mse(output, target)
+            
+        
+            
+        size=target.data.size()[0]
+        num+=size
+            
+        totloss+=loss.data.numpy()*size
+            
+           
+    meanloss=np.mean(totloss/num)
+    rmse=np.sqrt(meanloss)
+    print 'test-','MSE:',meanloss,'RMSE', rmse,'\n'
         
         
     
