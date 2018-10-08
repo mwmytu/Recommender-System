95e1d0a7336686e7b0f6ca0327f80a61c046d427
Wenming Ma <mwmcnpc@126.com>
2018-10-08 11:44:44 +0800

    Add files via upload

--- Tutorial/Codes/PytorchMF/Net.py
+++ Tutorial/Codes/PytorchMF/Net.py
@@ -14,78 +14,125 @@ import pandas as pd
 from torch.utils.data import TensorDataset,Dataset, DataLoader
 import torch.optim as optim
 import numpy as np
+from numpy.random import RandomState
 
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         self.userNum=1000
         self.itemNum=1700
-        self.UFLen=8
-        self.IFLen=8
+        self.UFLen=3
+        self.IFLen=3
+        self.random_state = RandomState(1)
         self.uembeddings=nn.Embedding(self.userNum,self.UFLen)
+        #torch.nn.init.uniform_(self.uembeddings.weight.data, a=0, b=1)
+        
         self.iembeddings=nn.Embedding(self.itemNum,self.IFLen)
+        #torch.nn.init.uniform_(self.iembeddings.weight.data, a=0, b=1)
+        
+        self.ubiase=nn.Embedding(self.userNum,1)
+        self.ibiase=nn.Embedding(self.itemNum,1)
+        #self.ufact=torch.rand(self.userNum,self.UFLen,requires_grad=True)
+        #self.ifact=torch.rand(self.itemNum,self.IFLen,requires_grad=True)
+        #self.ub=torch.rand(self.userNum,1,requires_grad=True)
+        #self.ib=torch.rand(self.itemNum,1,requires_grad=True)
+        #self.pars=[self.ufact,self.ifact,self.ub,self.ib]
     def forward(self, users,items):
         uembs=self.uembeddings(users)
-        iembs=self.uembeddings(users)
-        score=(uembs*iembs).sum(1)        
+        iembs=self.iembeddings(items)
+        ubias=self.ubiase(users)
+        ibias=self.ibiase(items)
+        score=(uembs*iembs).sum(1) +ubias.view(ubias.numel())+ibias.view(ibias.numel())       
+        #score=(uembs*iembs).sum(1)     
+        
+        #ufact=self.ufact[users,:]
+        #ifact=self.ifact[items,:]
+        #ub=self.ub[users,:]
+        #ib=self.ib[items,:]
+        #score=(ufact*ifact).sum(1)+ub.view(ub.numel())+ib.view(ib.numel())
+        
         return score
 
 def loadData(path):
-    data = pd.DataFrame(pd.read_table(path,header=None))
-    users=torch.tensor(data.iloc[:,0].values)-1
-    items=torch.tensor(data.iloc[:,0].values)-1
+    data = pd.DataFrame(pd.read_table(path,header=None,sep='\t',names="user_id,item_id,rating,timestamp".split(",") ))
+    #data.user_id = data.user_id.astype('category').cat.codes.values
+    #data.item_id = data.item_id.astype('category').cat.codes.values
+    #print data.head()
+    users=torch.tensor(data.iloc[:,0].values,dtype=torch.long)
+    #print users[1:5]
+    items=torch.tensor(data.iloc[:,1].values,dtype=torch.long)
+    #print len(users.unique()),len(items.unique())
     scores=torch.tensor(data.iloc[:,2].values,dtype=torch.float32)    
     dataset = TensorDataset(users,items, scores)
-    loader = DataLoader(dataset, batch_size = 16, shuffle = True)
+    loader = DataLoader(dataset, batch_size = 32, shuffle = True)
     return loader
 
 if __name__ == "__main__":
     net=Net()
     train_loader=loadData('./ml-100k/u1.base')
-    test_loader=loadData('./ml-100k/u1.base')
-    optimizer = optim.SGD(net.parameters(), lr=0.005)
+    test_loader=loadData('./ml-100k/u1.test')
+    #print net.parameters()
+    optimizer = optim.Adam(net.parameters(), lr=0.001,weight_decay=1e-4)
     index=0
-    #lambda_l2=0.001
-    mse = nn.MSELoss()
-    for epoch in range (1,50):
-        totloss=0
+    lambda_l2=0.1
+    mse = nn.MSELoss(size_average=True)
+    mae=nn.L1Loss(size_average=True)
+    for epoch in range (1,100):
+        tmse_loss=0.0
+        tmae_loss=0.0
         num=0
+        print epoch
         for i_batch, sample_batched in enumerate(train_loader):        
             optimizer.zero_grad()   # zero the gradient buffers
+            #print sample_batched[0],sample_batched[1]
             output=net(sample_batched[0],sample_batched[1])
+            #print output
             target=sample_batched[2]
-            loss = mse(output, target)
             
+            mseloss = mse(output, target)
+            maeloss=mae(output,target)
+            #print loss.data,'\n'
+            '''
+            regL2Loss=0.0
+            for W in net.parameters():
+                
+                regL2Loss+=torch.pow(W,2).sum()
+            loss=mseloss+regL2Loss*1e-5
+            '''
+            loss=mseloss
             loss.backward()
             optimizer.step()    # Does the update
             
-            size=target.data.size()[0]
-            num+=size
+            tmse_loss+=mseloss.data.numpy()
+            tmae_loss+=maeloss.data.numpy()
+            num+=1
+            #sprint totloss/num
             
-            totloss+=loss.data.numpy()*size
-            
-           
-        meanloss=np.mean(totloss/num)
-        rmse=np.sqrt(meanloss)
-        print meanloss, rmse,'\n'
-    totloss=0
+        #print len(train_loader) 
+        tmse=tmse_loss/num
+        rmse=np.sqrt(tmse)
+        tmae=tmae_loss/num
+        print tmse,rmse,tmae,'\n'
+    tmse_loss=0.0
+    tmae_loss=0.0
     num=0
     for i_batch, sample_batched in enumerate(test_loader):        
         output=net(sample_batched[0],sample_batched[1])
         target=sample_batched[2]
-        loss = mse(output, target)
+        mseloss = mse(output, target)
+        maeloss=mae(output,target)
+    
             
-        
+        tmse_loss+=mseloss.data.numpy()
+        tmae_loss+=maeloss.data.numpy()
+        num+=1
+            #sprint totloss/num
             
-        size=target.data.size()[0]
-        num+=size
-            
-        totloss+=loss.data.numpy()*size
-            
-           
-    meanloss=np.mean(totloss/num)
-    rmse=np.sqrt(meanloss)
-    print 'test-','MSE:',meanloss,'RMSE', rmse,'\n'
+        #print len(train_loader) 
+    tmse=tmse_loss/num
+    rmse=np.sqrt(tmse)
+    tmae=tmae_loss/num
+    print tmse,rmse,tmae,'\n'
         
         
     
